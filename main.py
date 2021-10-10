c5904c0467a8efa23f95a39b988663c5eb9e3214
Jenaro <jvega008@ucr.edu>
2021-10-10 14:15:11 -0700

    Added environment variables

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 import requests
 import smtplib
+import os
 
-OWN_EMAIL = email
-OWN_PASSWORD = password
+OWN_EMAIL = os.environ.get("EMAIL")
+OWN_PASSWORD = os.environ.get("PASSWORD")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=30, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
