717ce0c4adda1320886311366f7632f089d73689
Jenaro <jvega008@ucr.edu>
2021-10-10 18:56:21 -0700

    form fix 1

--- main.py
+++ main.py
@@ -183,30 +183,16 @@ def about():
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        # data = request.form
-        data = request.form
-        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
 
-def send_email(name, email, phone, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+# def send_email(name, email, phone, message):
+#     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
     # with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
     #     connection.starttls()
     #     connection.login(OWN_EMAIL, OWN_PASSWORD)
     #     connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
-    r = requests.post("https://api.mailgun.net/v2/%s/messages" % app.config['MAILGUN_DOMAIN'],
-             auth=("api", app.config['MAILGUN_KEY']),
-             data={
-                 "from": from_address,
-                 "to": to_address,
-                 "subject": subject,
-                 "text": plaintext,
-                 "html": html
-             }
-             )
-    return r
 
 
 @app.route("/new-post", methods=["GET", "POST"])
